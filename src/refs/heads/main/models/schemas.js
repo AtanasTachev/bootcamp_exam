5e115a0f5f919f5dec600489d1aac8df7f2cb8d7
Atanas Tachev <tachev_a#yahoo.com>
2021-08-29 09:39:22 +0300

    schemas

---
+++ src/refs/heads/main/models/schemas.js
@@ -0,0 +1,51 @@
+const schemas = {
+   createJob: {
+      title: String,
+      description: String
+},
+    updateJob: {
+    title: String,
+    description: String
+   },
+    job: {
+    _id: String,
+    title: String,
+    description: String
+    },
+    detailedJob: [{
+        allOf: {
+            candidates:[],
+            jobs: [],
+            jobCandidatesList:[],
+            candidatesList: [],
+            candidate: {
+                firstName: String,
+                lastName: String,
+                email: String
+            },
+            candidateCreate: {
+                firstName: String,
+                lastName: String,
+                email: String
+            },
+            candidateUpdate: {
+                firstName: String,
+                lastName: String,
+                email: String
+            }
+        },
+        interview: {
+            _id: String,
+            jobId: String,
+            candidateId: String,
+            slot: Number
+        },
+        interviewUpdate: {
+            jobId: String,
+            candidateId: String,
+            slot: Number,
+
+        },
+        jobInterviewsList: []
+    }]
+}
