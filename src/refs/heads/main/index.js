bc32619380e343188790e12d140e8f223ed845b2
Atanas Tachev <tachev_a#yahoo.com>
2021-09-02 21:16:41 +0300

    many changes
    many changes

---
+++ src/refs/heads/main/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import './index.css';
+import App from './App.js'
+import reportWebVitals from './reportWebVitals';
+
+ReactDom.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+reportWebVitals();
